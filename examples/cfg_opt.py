dded01f1f15b5c494b437aeb64d1801e50417f31
qh53 <qh53@cornell.edu>
2019-10-22 01:27:36 -0400

    cross jump

--- examples/cfg_opt.py
+++ examples/cfg_opt.py
@@ -4,6 +4,7 @@ from df import df_worklist, ANALYSES, fmt, interp
 from form_blocks import form_blocks
 import cfg
 from cfg_to_code import cfg_to_code
+from util import fresh
 
 def br_removal(blocks):
     in_, out = df_worklist(blocks, ANALYSES['cprop'])
@@ -110,6 +111,128 @@ def unreachable_removal(blocks):
     return blocks
 
 
+def canonicalize(instr):
+    if instr['op'] in ['add', 'mul', 'and', 'or']:
+        instr['args'] = sorted(instr['args'])
+    return json.dumps(instr, sort_keys=True)
+
+def merge(block1, block2, names, from_start):
+    l = 0
+    for i in range(len(block1)):
+        if from_start:
+            instr1 = block1[l]
+            instr2 = block2[l]
+        else:
+            instr1 = block1[len(block1) - l - 1]
+            instr2 = block2[len(block2) - l - 1]
+        if canonicalize(instr1) != canonicalize(instr2):
+            break
+        l +=1
+    return fresh('t', names), l
+
+def replace_target(blocks_r, all_blocks, orginals, new):
+    for p in blocks_r:
+        new_args = []
+        for idx, arg in enumerate(all_blocks[p][-1]["args"]):
+            if len(blocks[p][-1]["args"]) > 1 and idx == 0:
+                # br gurad
+                new_args.append(arg)
+            elif arg in orginals:
+                new_args.append(new)
+            else:
+                new_args.append(arg)
+        all_blocks[p][-1]["args"] = new_args
+
+def tail_merging_once(blocks):
+    preds, succs = cfg.edges(blocks)
+   
+    bns = list(blocks.keys())
+    for block in bns:
+        count = {}
+        # merge two preds
+       
+        for p in preds[block]:
+            instr = blocks[p][-1]
+            if instr is not None and instr['op'] == 'jmp':
+                k = canonicalize(instr) 
+                if k not in count:
+                    count[k] = p
+                else:
+                    # find two blocks can be merged
+                    b1 = blocks[count[k]]
+                    b2 = blocks[p]
+                    name, l = merge(b1, b2, list(blocks.keys()), from_start = False)
+                    if l == 1:
+                        continue
+                    
+                    if l == min(len(b1),len(b2)):
+                        if len(b1) == len(b2):
+                            replace_target(preds[count[k]] +  preds[p], blocks, [count[k], p], name)
+                            del blocks[count[k]]
+                            del blocks[p]
+                            return True
+                        if len(b1) < len(b2):
+                            small_block = count[k]
+                            large_block = p
+                        else:   
+                            small_block = p
+                            large_block = count[k]
+
+                        blocks[large_block] = blocks[large_block][:len(blocks[large_block]) - l]
+                        jmp = {'op': 'jmp', "args": [small_block]} 
+                        blocks[large_block].append(jmp)
+
+                    else:
+                        jmp = {'op': 'jmp', "args": [name]} 
+                        new_block = b1[len(b1) - l:]
+                        blocks[name] = new_block
+                        blocks[count[k]] = b1[:len(b1) - l]
+                        blocks[count[k]].append(jmp)
+                        blocks[p] = b2[:len(b2) - l]
+                        blocks[p].append(jmp)
+                    return True                   
+                    
+
+        # merge two succs
+        if blocks[block][-1] is not None and blocks[block][-1]['op'] == 'br':
+            args = blocks[block][-1]['args']
+            b1 = blocks[args[1]]
+            b2 = blocks[args[2]]
+            name, l = merge(b1, b2, list(blocks.keys()),from_start = True)
+            if l > 0:  
+                if l == len(b1):
+                    # cannot have jmp in middle of block
+                    # so this is the only case such that a block is entirely duplicated
+                    jmp = {'op': 'jmp', "args": [args[1]]}
+                    blocks[block] = blocks[block][:-1]
+                    blocks[block].append(jmp)
+                    replace_target(preds[args[2]], blocks, [args[2]], args[1])
+                    del blocks[args[2]]
+                else:
+                    jmp = {'op': 'jmp', "args": [name]}
+                    br = blocks[block][-1]
+                    blocks[block] = blocks[block][:-1]
+                    blocks[block].append(jmp)
+                    replace_target(preds[args[1]] +  preds[args[2]], blocks, args[1:3], name)
+                    blocks[name] = b1[:l]
+                    blocks[args[1]] = b1[l:]
+                    blocks[args[2]] = b2[l:]
+                    blocks[name].append(br)
+                return True
+    
+
+    return False
+
+def tail_merging(blocks):
+
+    while(tail_merging_once(blocks)):
+        pass
+        # print("=======================")
+        # for block in blocks:
+        #     print('{}:'.format(block))
+        #     print('{}:'.format(blocks[block]))
+    return blocks
+
 if __name__ == '__main__':
     bril = json.load(sys.stdin)
     for func in bril['functions']:
@@ -122,22 +245,33 @@ if __name__ == '__main__':
         #     print('{}:'.format(block))
         #     print('{}:'.format(blocks[block]))
 
-        br_removal(blocks)
+        # br_removal(blocks)
         # print("================after br_removal ==================")
         # for block in blocks:
         #     print('{}:'.format(block))
         #     print('{}:'.format(blocks[block]))
 
-        unreachable_removal(blocks)
+        # unreachable_removal(blocks)
         # print("================after unreachable_removal ==================")
         # for block in blocks:
         #     print('{}:'.format(block))
         #     print('{}:'.format(blocks[block]))
 
-        clean_cfg(blocks)
+        # clean_cfg(blocks)
         # print("================after clean_cfg ==================")
         # for block in blocks:
         #     print('{}:'.format(block))
         #     print('{}:'.format(blocks[block]))
 
+        tail_merging(blocks)
+        # print("================after tail_merging ==================")
+        # for block in blocks:
+        #     print('{}:'.format(block))
+        #     print('{}:'.format(blocks[block]))
+
+        # clean_cfg(blocks)
+        # print("================after clean_cfg ==================")
+        # for block in blocks:
+        #     print('{}:'.format(block))
+        #     print('{}:'.format(blocks[block]))
         print(cfg_to_code(blocks, func['name']))
--- examples/cfg_to_code.py
+++ examples/cfg_to_code.py
@@ -1,16 +1,20 @@
 import json
 import cfg
 
-def cfg_to_code(blocks, name):
+def cfg_to_code(blocks, f_name):
     preds, succs = cfg.edges(blocks)
     instrs = []
     for name, block in blocks.items():
         if len(instrs) != 0 and instrs[-1]['op'] == 'jmp' and instrs[-1]['args'][0] == name:
             instrs = instrs[:-1]
+            if len(preds[name]) == 1:
+                # this label is only for the fall through
+                instrs+= block
+                continue
         if len(preds[name]) != 0:
             instrs += [{"label": name}] 
         instrs+= block
-    data = {'functions': [{"name": name}]}
-    data['functions'][0]['instrs'] = instrs[:-1]
+    data = {'functions': [{"name": f_name}]}
+    data['functions'][0]['instrs'] = instrs
     json_data = json.dumps(data, indent=2, sort_keys=True)
     return json_data
